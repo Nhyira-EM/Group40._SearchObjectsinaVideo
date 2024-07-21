094c1a901e804db3a4cb79f27b583e6b70ccdcc6
Nhyira <efreduahagyemang@gmail.com>
2024-07-21 19:09:31 +0000

    Added comments

--- homework3_search_objects_in_a_video_footage_ipnyb.py
+++ homework3_search_objects_in_a_video_footage_ipnyb.py
@@ -11,20 +11,26 @@ import matplotlib.pyplot as plt
 # Function to extract frames from the uploaded video
 def get_frames(uploaded_file):
     if not uploaded_file:
+        # If no file is uploaded, show a warning
         st.warning("No file uploaded")
         return None
     else:
+        # If a file is uploaded, save it to the current directory
         filename = uploaded_file.name
         with open(filename, mode='wb') as f:
             f.write(uploaded_file.getbuffer())
         
+        # Create a directory to store frames
         if os.path.exists('frames'):
             shutil.rmtree('frames')
         os.makedirs('frames', exist_ok=True)
         
+        # Open the video file
         cap = cv2.VideoCapture(filename)
         frame_count = 0
         frame_filenames = []
+        
+        #Reading frames from the video
         while True:
             ret, frame = cap.read()
             if not ret:
@@ -33,7 +39,10 @@ def get_frames(uploaded_file):
             frame_filename = f'frames/frame_{frame_count}.jpg'
             cv2.imwrite(frame_filename, frame)
             frame_filenames.append(frame_filename)
+        # Release the video capture object
         cap.release()
+        
+        # Show a message based on the number of frames extracted
         if frame_count == 0:
             st.warning("No frames extracted. Please check the video file.")
         else:
@@ -43,11 +52,15 @@ def get_frames(uploaded_file):
 # Function to detect objects in the frames
 def detect_all_objects(frame_filenames):
     if not frame_filenames:
+        # If no frames are found, show a warning
         st.warning("No frames found")
         return None, None
     else:
+        # Loading the pre-trained InceptionV3 model
         model = InceptionV3(weights='imagenet', include_top=True)
         frame_obj_dict = {}
+
+        #Iterating through each frame and make predictions
         for frame_filename in frame_filenames:
             img = tf.io.read_file(frame_filename)
             img = tf.image.decode_image(img, channels=3)
@@ -58,14 +71,18 @@ def detect_all_objects(frame_filenames):
             prediction = model.predict(img_expanded, verbose=0)
             decoded_predictions = tf.keras.applications.inception_v3.decode_predictions(prediction, top=3)
             img_dict = {}
+            
+            #Storing the top-3 predicted labels for each frame
             for i, (imagenet_id, label, score) in enumerate(decoded_predictions[0]):
                 img_dict[i + 1] = label
             frame_obj_dict[frame_filename] = img_dict
 
+        #Collecting all detected objects across frames
         all_objects = []
         for frame_name in frame_obj_dict.keys():
             for obj_id in frame_obj_dict[frame_name].keys():
                 all_objects.append(frame_obj_dict[frame_name][obj_id])
+        # Get a list of unique objects detected
         all_obj = list(set(all_objects))
 
         return frame_obj_dict, all_obj
@@ -73,20 +90,25 @@ def detect_all_objects(frame_filenames):
 # Function to search for a specific object in the frames
 def search_object(search_query, frame_obj_dict, all_obj):
     if not frame_obj_dict:
+        # If no frames are found, show a warning
         st.warning("No frames found")
         return None
     else:
         obj_frames = []
+
+        #Searching for the object in the frames
         for frame_name in frame_obj_dict.keys():
             if search_query in frame_obj_dict[frame_name].values():
                 obj_frames.append(frame_name)
 
         if len(obj_frames) == 0:
+            # If the object is not found, show a warning and list all detected objects
             st.warning("Object doesn't exist!")
             st.write("Choose from the list below:")
             for obj in all_obj:
                 st.write(obj)
         else:
+            # Display frames containing the searched object
             for framee in obj_frames:
                 frame_path = framee
                 frame = cv2.imread(frame_path)
@@ -95,13 +117,18 @@ def search_object(search_query, frame_obj_dict, all_obj):
 # Streamlit app layout
 st.title("Object Detection in Video Frames")
 
+# Upload a video file
 uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
+
+# Search for an object in the video frames
 search_query = st.text_input("Enter the object to search for:")
 
 if st.button("Search"):
     if uploaded_file and search_query:
+        # Extract frames from the uploaded video
         frame_filenames = get_frames(uploaded_file)
         if frame_filenames:
+            # Detect objects in the frames and search for the object
             frame_obj_dict, all_obj = detect_all_objects(frame_filenames)
             search_object(search_query, frame_obj_dict, all_obj)
     elif not uploaded_file:
